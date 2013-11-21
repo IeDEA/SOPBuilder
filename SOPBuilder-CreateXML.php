e03d0395ee07097ef72ce9f6223e7ff9cb38df9f
Anneliese Moosman <anneliese.moosman@vanderbilt.edu>
2013-11-21 16:30:07 -0600

    xml output updates

--- SOPBuilder-CreateXML.php
+++ SOPBuilder-CreateXML.php
@@ -1,20 +1,46 @@
 <?php
+include 'SOPBuilder-Functions.php';
 
-$xml = new SimpleXMLElement('<xml/>');
-$study = $xml->addChild('Study');
-$study->addAttribute('name', "HICDEP 1.60");
-$study->addAttribute('version', "1.60");
-$study->addAttribute('projectID', "HICDEP");
-$study->addAttribute('xsi:noNamespaceSchemaLocation', "study.xsd");
+// temporarilly assign variables
+$study_projectID = "HICDEP";
+$study_version = "1.60";
+$project_name = "All HICDEP tables";
+$project_projectID = "ALL_HICDEP";
+
+
+// get all tables to include
+$query = explode(",",$_POST['query']);
+$tables = array();
+foreach($query as $tableIvar){
+    $tmp = explode("/",$tableIvar);
+    $tables[$tmp[0]] = $tmp[0];
+}
+$tables = array_values($tables);
+
+$study = new ExSimpleXMLElement('<Study></Study>');
+$study->addAttribute('name', $study_projectID." ".$study_version);
+$study->addAttribute('version', $study_version);
+$study->addAttribute('projectID', $study_projectID);
+$study->addAttribute('xsi:noNamespaceSchemaLocation',"study.xsd",'xsi:noNamespaceSchemaLocation');
+//$study->addAttribute("xmlns:ss:Type", "String");
 $project = $study->addChild('Project');
-$project->addAttribute('name','All HICPEP tables');
-$project->addAttribute('projectID','ALL_HICDEP');
+$project->addAttribute('name',$project_name);
+$project->addAttribute('projectID',$project_projectID);
 $inclusionCriteria = $project->addChild('InclusionCriteria');
 $inclusionCriteria->addChild('description');
-$inclusionCriteria->addChild('SQL','<![CDATA[
-      Select Patient from tblBAS
-      ]]>');
+$inclusionCriteria->addChildCData('SQL','Select Patient from tblBAS');
+$tableList = $project->addChild('Tablelist');
+foreach($tables as $table_name){
+    $table = $tableList->addChild('Table');
+    $table->addAttribute('TableName',$table_name);
+}
 
 
-Header('Content-type: text/xml');
-print($xml->asXML());
+$file_name = "study_".$study_projectID.$study_version.".xml";
+header('Content-type: text/xml');
+header('Content-Disposition: inline; filename="'.$file_name.'"');
+$dom = new DOMDocument('1.0');
+$dom->preserveWhiteSpace = false;
+$dom->formatOutput = true;
+$dom->loadXML($study->asXML());
+echo $dom->saveXML();
